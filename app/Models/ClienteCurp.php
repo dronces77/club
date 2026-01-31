bfb4dc4d926c5c188967d513dd7bf626664d65d0
dronces77 <dronces@gmail.com>
2026-01-31 17:20:23 -0600

    Update ClienteCurp.php

--- app/Models/ClienteCurp.php
+++ app/Models/ClienteCurp.php
@@ -4,19 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ClienteCurp extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'clientes_curp';
     
-    protected $fillable = ['cliente_id', 'curp', 'es_principal'];
+    // CONSTANTES CORRECTAS
+    const CREATED_AT = 'creado_en';
+    const UPDATED_AT = null; // No tenemos columna updated_at
     
+    protected $fillable = [
+        'cliente_id',
+        'curp',
+        'es_principal'
+    ];
+
     protected $casts = [
         'es_principal' => 'boolean',
+        'creado_en' => 'datetime'
     ];
-    
+
     public function cliente()
     {
         return $this->belongsTo(Cliente::class);
