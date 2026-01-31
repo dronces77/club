72af1e3c50506ad7b82f1f4c94c822dde1233ab1
dronces77 <dronces@gmail.com>
2026-01-31 17:21:03 -0600

    Update ClienteRfc.php

--- app/Models/ClienteRfc.php
+++ app/Models/ClienteRfc.php
@@ -4,19 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ClienteRfc extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'clientes_rfc';
     
-    protected $fillable = ['cliente_id', 'rfc', 'es_principal'];
+    const CREATED_AT = 'creado_en';
+    const UPDATED_AT = null;
     
+    protected $fillable = [
+        'cliente_id',
+        'rfc',
+        'es_principal'
+    ];
+
     protected $casts = [
         'es_principal' => 'boolean',
+        'creado_en' => 'datetime'
     ];
-    
+
     public function cliente()
     {
         return $this->belongsTo(Cliente::class);
