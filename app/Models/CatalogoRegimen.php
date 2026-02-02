a54309570e5131af25fcb68f81294b5ac497cee8
dronces77 <dronces@gmail.com>
2026-02-02 17:11:29 -0600

    Update CatalogoRegimen.php

--- app/Models/CatalogoRegimen.php
+++ app/Models/CatalogoRegimen.php
@@ -4,23 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CatalogoRegimen extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'catalogo_regimenes';
     protected $primaryKey = 'id';
-    
+
     protected $fillable = ['instituto_id', 'codigo', 'nombre'];
-    
-    // Relación con instituto
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
     public function instituto()
     {
         return $this->belongsTo(CatalogoInstituto::class, 'instituto_id');
     }
-    
-    // Relación con clientes
+
     public function clientes()
     {
         return $this->hasMany(Cliente::class, 'regimen_id');
